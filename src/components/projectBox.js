a16bb4b76925aa6f0c13f2fa96b775eec2a73ad5
Arijit Chakma <ac4393@drexel.edu>
2024-09-07 02:04:29 -0400

    added two projects and fixed project card

--- src/components/projectBox.js
+++ src/components/projectBox.js
@@ -1,28 +1,20 @@
 import React from 'react';
 import '../styles/project.css';
 
-const ProjectBox = ({ name, date, link, description, image, skills }) => {
+const ProjectBox = ({ name, link, description, image }) => {
   return (
     <a href={link} className="project-card-link" target="_blank" rel="noopener noreferrer">
       <div className="project-card">
-        {/* Single Image */}
+        {/* Image */}
         <div className="project-image">
           <img src={image} alt={`${name} screenshot`} />
         </div>
 
+        {/* Project Name */}
         <h2 className="project-name">{name}</h2>
-        <p className="project-date">Date: {date}</p>
-        <p className="project-description">{description}</p>
 
-        {/* Skills */}
-        <div className="project-skills">
-          <h4>Skills Used:</h4>
-          <ul>
-            {skills.map((skill, index) => (
-              <li key={index}>{skill}</li>
-            ))}
-          </ul>
-        </div>
+        {/* Project Description */}
+        <p className="project-description">{description}</p>
       </div>
     </a>
   );
--- src/pages/project.js
+++ src/pages/project.js
@@ -3,21 +3,20 @@ import ProjectBox from '../components/projectBox';
 import '../styles/project.css'; 
 import projectsData from '../assets/projects.json'
 
+
 const ProjectsPage = () => {
     return (
       <div>
-        <h1>Projects</h1>
-        <div className="projects-grid">
+       <div className="projects-grid">
           {/* Loop through the JSON data and render a Project component for each item */}
           {projectsData.map((project, index) => (
             <ProjectBox
-              key={index} // Provide a unique key for each project
+              key={index} 
               name={project.name}
               date={project.date}
               link={project.link}
               description={project.description}
               image={project.image}
-              skills={project.skills}
             />
           ))}
         </div>
